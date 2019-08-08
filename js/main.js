8b1ac26cee2294f7833e9c368fb0321696a8568c
Brian Han <bmhan319@hotmail.com>
2019-08-08 15:58:47 -0700

    1st attempt at getting carousel svg to load

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ $(document).ready(function() {
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: "40px",
+          centerPadding: "0px",
           slidesToShow: 1
         }
       }
